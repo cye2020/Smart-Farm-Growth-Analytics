2cdcd4e050b33d4186c038538af144f24a0a55f8
cye2020 <ariaplay101@gmail.com>
2025-11-04 17:24:10 +0900

    feat: ttest init 추가

--- code/utils/__init__.py
+++ code/utils/__init__.py
@@ -1,10 +1,11 @@
 from .config import PROJECT_DIR, CODE_DIR, DATA_DIR, MODEL_DIR, \
     growth_map, energy_map
 from .eda import eda_missing_data, eda_duplicates, plot_features
-
+from .statistic import TTest
 
 __all__ = [
     'PROJECT_DIR', 'CODE_DIR', 'DATA_DIR', 'MODEL_DIR',
     'growth_map', 'energy_map',
-    'eda_missing_data', 'eda_duplicates', 'plot_features'
+    'eda_missing_data', 'eda_duplicates', 'plot_features',
+    'TTest'
 ]
