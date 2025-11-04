fc89d2025ac0807f4ce836ca994bb9b82975ac0d
cye2020 <79790831+cye2020@users.noreply.github.com>
2025-11-04 17:24:53 +0900

    feat: ttest init 추가 (#2)
    * feat: t검정 코드 추가
    
    * feat: ttest init 추가

--- code/__init__.py
+++ code/__init__.py
@@ -1,10 +1,12 @@
 from .utils import PROJECT_DIR, CODE_DIR, DATA_DIR, MODEL_DIR, \
     growth_map, \
-    eda_missing_data, eda_duplicates, plot_features
+    eda_missing_data, eda_duplicates, plot_features, \
+    TTest
 
 
 __all__ = [
     'PROJECT_DIR', 'CODE_DIR', 'DATA_DIR', 'MODEL_DIR',
     'growth_map',
-    'eda_missing_data', 'eda_duplicates', 'plot_features'
+    'eda_missing_data', 'eda_duplicates', 'plot_features',
+    'TTest'
 ]
